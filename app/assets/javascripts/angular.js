8922bb916a0cb7c97c880663ab028a42a41d5b97
John Kim <johntkim92@gmail.com>
2015-11-27 18:02:41 -0500

    posts rendering through angular now

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -2,11 +2,19 @@ var app = angular.module('YodaApp', []);
 
 //Post Controller
 app.controller('PostsController', ['$http', function($http){
+  var controller = this;
+  // get all posts
+  $http.get('/posts').success(function(data){
+    controller.posts = data.posts;
+  });
+
   this.createPost = function() {
     $http.post('/posts', {
       post: {
-        title:
+        title: 'bruh'
       }
+    }).success(function(data){
+      console.log(data);
     });
   }
 }]);
