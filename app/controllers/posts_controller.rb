69ace92979e0ee060d07d43434936db1ff30d54b
John Kim <johntkim92@gmail.com>
2015-11-27 15:38:08 -0500

    added in all necessary methods for posts, now to move onto jbuilder

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
 
+  include PostsHelper
+
   def index
     @posts = Post.all
   end
@@ -7,5 +9,36 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+
+    redirect_to post_path(@post)
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+
+    flash.notice = "Post '#{@post.title}' was updated!"
+
+    redirect_to post_path(@post)
+  end
+
 
 end
